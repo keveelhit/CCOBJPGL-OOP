858880a62fb40ceeaec4bb5349efdc25bb3157dc
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-04-22 21:55:51 +0800

    Create BookInformation.java

---
+++ source code dumpster/BookInformation.java
@@ -0,0 +1,28 @@
+package LibrarySystemLoginFeature;
+
+public class BookInformation {
+
+	String name;
+	String author;
+	int copy;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getCopy() {
+		return copy;
+	}
+	public void setCopy(int copy) {
+		this.copy = copy;
+	} 
+	
+}
