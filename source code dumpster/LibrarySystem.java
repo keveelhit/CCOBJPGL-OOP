5bc53abbfb2dd1938359670af96e2266e1668287
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-04-23 16:51:26 +0800

    Update LibrarySystem.java

--- source code dumpster/LibrarySystem.java
+++ source code dumpster/LibrarySystem.java
@@ -62,27 +62,72 @@ public class LibrarySystem {
 		
 		JFrame adminLoginFrame = new JFrame("Login as an Admin");
 		JPanel adminFramePanel = new JPanel();
-		JLabel adminUsername = new JLabel();
-		JLabel adminPassword = new JLabel();
+		JLabel adminText = new JLabel("ADMIN PAGE");
+		JLabel adminUsername = new JLabel("Username:");
+		JLabel adminPassword = new JLabel("Password:");
 		JTextField adminUserInput = new JTextField();
 		JPasswordField adminPassInput = new JPasswordField();
+		JButton loginButton = new JButton("Login");
 		
+		// Label Settings
+		adminText.setBounds(150, 30, 200, 40);
+		adminText.setFont(new Font("Roboto",Font.BOLD, 14));
+		adminUsername.setBounds(50, 95, 80, 25);
+		adminPassword.setBounds(50, 140, 80, 25);
+		
+		// TextField and PasswordField Settings
+		adminUserInput.setLayout(null);
+		adminUserInput.setBounds(120, 95, 200, 25);
+		adminPassInput.setLayout(null);
+		adminPassInput.setBounds(120, 140, 200, 25);
+		
+		loginButton.setLayout(null);
+		loginButton.setBounds(145, 190, 150, 40);
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = adminUserInput.getText();
+				String password = adminPassInput.getText();
+				
+				if ((username.equals("Admin_01") && password.equals("AdminOne")) || (username.equals("Admin_02") && password.equals("AdminTwo")) || (username.equals("Admin_03") && password.equals("AdminThree"))) {
+					JOptionPane.showMessageDialog(adminLoginFrame, "SUCCESFULLY LOGIN");
+					adminLoginFrame.dispose();
+					adminModule();
+
+				} else if ((!username.equals("Admin_01") && password.equals("AdminOne")) || (!username.equals("Admin_02") && password.equals("AdminTwo")) || (!username.equals("Admin_03") && password.equals("AdminThree"))) {
+					JOptionPane.showMessageDialog(adminLoginFrame, "INCORRECT USERNAME.");
+					
+				} else if ((username.equals("Admin_01") && !password.equals("AdminOne")) || (username.equals("Admin_02") && !password.equals("AdminTwo")) || (username.equals("Admin_03") && !password.equals("AdminThree"))) {
+					JOptionPane.showMessageDialog(adminLoginFrame, "INCORRECT PASSWORD.");
+					
+				} else if ((!username.equals("Admin_01") && !password.equals("AdminOne")) || (!username.equals("Admin_02") && !password.equals("AdminTwo")) || (!username.equals("Admin_03") && !password.equals("AdminThree"))) {
+					JOptionPane.showMessageDialog(adminLoginFrame, "INCORRECT USERNAME and PASSWORD.");
+					
+				} 
+			}
+		});
+		
+		// Frame Panel Settings
 		adminFramePanel.setLayout(null);
+		adminFramePanel.add(adminText);
 		adminFramePanel.add(adminUsername);
 		adminFramePanel.add(adminPassword);
 		adminFramePanel.add(adminUserInput);
 		adminFramePanel.add(adminPassInput);
+		adminFramePanel.add(loginButton);
 		
+		// Admin Frame Settings
 		adminLoginFrame.setLocation(400, 100);
 		adminLoginFrame.setSize(400, 300);
 		adminLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		adminLoginFrame.setLayout(null);
 		adminLoginFrame.setVisible(true);
 		adminLoginFrame.add(adminFramePanel);
+		adminLoginFrame.add(adminText);
 		adminLoginFrame.add(adminUsername);
 		adminLoginFrame.add(adminPassword);
 		adminLoginFrame.add(adminUserInput);
 		adminLoginFrame.add(adminPassInput);
+		adminLoginFrame.add(loginButton);
 		
 	}
 	
@@ -90,27 +135,80 @@ public class LibrarySystem {
 		
 		JFrame librarianLoginFrame = new JFrame("Login as a Librarian");
 		JPanel librarianFramePanel = new JPanel();
-		JLabel librarianUsername = new JLabel();
-		JLabel librarianPassword = new JLabel();
+		JLabel librarianText = new JLabel("LIBRARIAN PAGE");
+		JLabel librarianUsername = new JLabel("Username:");
+		JLabel librarianPassword = new JLabel("Password:");
 		JTextField librarianUserInput = new JTextField();
 		JPasswordField librarianPassInput = new JPasswordField();
+		JButton loginButton = new JButton("Login");
 		
+		// Label Settings
+		librarianText.setBounds(140, 30, 200, 40);
+		librarianText.setFont(new Font("Roboto",Font.BOLD, 14));
+		librarianUsername.setBounds(50, 95, 80, 25);
+		librarianPassword.setBounds(50, 140, 80, 25);
+		
+		// TextField and PasswordField Settings
+		librarianUserInput.setLayout(null);
+		librarianUserInput.setBounds(120, 95, 200, 25);
+		librarianPassInput.setLayout(null);
+		librarianPassInput.setBounds(120, 140, 200, 25);
+		
+		loginButton.setLayout(null);
+		loginButton.setBounds(145, 190, 150, 40);
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = librarianUserInput.getText();
+				String password = librarianPassInput.getText();
+				
+				if ((username.equals("Librarian_01") && password.equals("LibOne")) || (username.equals("Librarian_02") && password.equals("LibTwo")) || (username.equals("Librarian_03") && password.equals("LibThree"))) {
+					JOptionPane.showMessageDialog(librarianLoginFrame, "SUCCESFULLY LOGIN");
+					librarianLoginFrame.dispose();
+					librarianModule();
+			
+				} else if ((!username.equals("Librarian_01") && password.equals("LibOne")) || (!username.equals("Librarian_02") && password.equals("LibTwo")) || (!username.equals("Librarian_03") && password.equals("LibThree"))) {
+					JOptionPane.showMessageDialog(librarianLoginFrame, "INCORRECT USERNAME.");
+					
+				} else if ((username.equals("Librarian_01") && !password.equals("LibOne")) || (username.equals("Librarian_02") && !password.equals("LibTwo")) || (username.equals("Librarian_03") && !password.equals("LibThree"))) {
+					JOptionPane.showMessageDialog(librarianLoginFrame, "INCORRECT PASSWORD.");
+					
+				} else if ((!username.equals("Librarian_01") && !password.equals("LibOne")) || (!username.equals("Librarian_02") && !password.equals("LibTwo")) || (!username.equals("Librarian_03") && !password.equals("LibThree"))) {
+					JOptionPane.showMessageDialog(librarianLoginFrame, "INCORRECT USERNAME and PASSWORD.");
+					
+				} 
+			}
+		});
+		
+		// Frame Panel Settings
 		librarianFramePanel.setLayout(null);
+		librarianFramePanel.add(librarianText);
 		librarianFramePanel.add(librarianUsername);
 		librarianFramePanel.add(librarianPassword);
 		librarianFramePanel.add(librarianUserInput);
 		librarianFramePanel.add(librarianPassInput);
+		librarianFramePanel.add(loginButton);
 		
+		// Librarian Frame Settings
 		librarianLoginFrame.setLocation(400, 100);
 		librarianLoginFrame.setSize(400, 300);
 		librarianLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		librarianLoginFrame.setLayout(null);
 		librarianLoginFrame.setVisible(true);
 		librarianLoginFrame.add(librarianFramePanel);
+		librarianLoginFrame.add(librarianText);
 		librarianLoginFrame.add(librarianUsername);
 		librarianLoginFrame.add(librarianPassword);
 		librarianLoginFrame.add(librarianUserInput);
 		librarianLoginFrame.add(librarianPassInput);
+		librarianLoginFrame.add(loginButton);
+		
+	}
+	
+	public static void adminModule() {
+		
+	}
+	
+	public static void librarianModule() {
 		
 	}
 	
