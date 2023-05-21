7006e6a32ef272256d72ee2568edf65402171ab5
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-05-22 02:25:36 +0800

    Create Vehicle.java

---
+++ Inheritance and Overriding Activity, Monday of May 22/Vehicle.java
@@ -0,0 +1,39 @@
+package InheritanceAndOverriding;
+
+/*
+ * Name: Karen Pearl V. Pabilando
+ * Section: INF224
+ * Course: CCOBJPGL
+ * Date: May 22, 2023
+ */
+
+// Base class: Vehicle
+class Vehicle {
+    private double speed;
+    private String color;
+    private double price;
+
+    public Vehicle(double speed, String color, double price) {
+        this.speed = speed;
+        this.color = color;
+        this.price = price;
+    }
+
+    public void stop() {
+        System.out.println("Vehicle is stopping.");
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+
