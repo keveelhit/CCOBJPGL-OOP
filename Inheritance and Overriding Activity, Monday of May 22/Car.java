66d57f4cfd02945f638179957dfa5265358028b1
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-05-22 02:26:06 +0800

    Create Car.java

---
+++ Inheritance and Overriding Activity, Monday of May 22/Car.java
@@ -0,0 +1,27 @@
+package InheritanceAndOverriding;
+
+/*
+ * Name: Karen Pearl V. Pabilando
+ * Section: INF224
+ * Course: CCOBJPGL
+ * Date: May 22, 2023
+ */
+
+// Derived class: Car
+class Car extends Vehicle {
+    private int numDoors;
+
+    public Car(double speed, String color, double price, int numDoors) {
+        super(speed, color, price);
+        this.numDoors = numDoors;
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car is stopping.");
+    }
+
+    public int getNumDoors() {
+        return numDoors;
+    }
+}
