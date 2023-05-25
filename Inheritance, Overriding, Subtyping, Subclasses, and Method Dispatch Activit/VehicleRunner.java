a7c58d48f87f6c2a92f65b249e0b28ed20c2ba7c
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-05-25 10:09:22 +0800

    Update VehicleRunner.java

--- Inheritance, Overriding, Subtyping, Subclasses, and Method Dispatch Activit/VehicleRunner.java
+++ Inheritance, Overriding, Subtyping, Subclasses, and Method Dispatch Activit/VehicleRunner.java
@@ -14,7 +14,7 @@ public class VehicleRunner {
 
          // Create a ToyotaVios Object
         ToyotaVios toyotaVios = new ToyotaVios(170.0, "Yellow", 706000.0, 4, "Radial");
-        System.out.println("Toyota Vios speed: " + toyotaVios.getSpeed() + " kmph");
+        System.out.println("Toyota Vios speed: " + toyotaVios.getSpeed() + " km/h");
         System.out.println("Toyota Vios color: " + toyotaVios.getColor());
         System.out.println("Toyota Vios price: " + toyotaVios.getPrice());
         System.out.println("Number of doors: " + toyotaVios.getNumDoors());
@@ -24,8 +24,8 @@ public class VehicleRunner {
         System.out.println();
 
         // Create a U2SpyPlane Object
-        U2SpyPlane u2SpyPlane = new U2SpyPlane(1500.0, "Red", 10000000.0, 80.0, "U-2");
-        System.out.println("U2 Spy Plane speed: " + u2SpyPlane.getSpeed());
+        U2SpyPlane u2SpyPlane = new U2SpyPlane(700.0, "Red", 10000000.0, 80.0, "U-2");
+        System.out.println("U2 Spy Plane speed: " + u2SpyPlane.getSpeed() + " km/h");
         System.out.println("U2 Spy Plane color: " + u2SpyPlane.getColor());
         System.out.println("U2 Spy Plane price: " + u2SpyPlane.getPrice());
         System.out.println("Wingspan: " + u2SpyPlane.getWingspan());
@@ -36,7 +36,7 @@ public class VehicleRunner {
 
         // Create a FandangoYacht Object
         FandangoYacht fandangoYacht = new FandangoYacht(10.0, "Blue", 8000000.0, "White", "Gray");
-        System.out.println("Fandango Yacht speed: " + fandangoYacht.getSpeed());
+        System.out.println("Fandango Yacht speed: " + fandangoYacht.getSpeed() + " mph");
         System.out.println("Fandango Yacht color: " + fandangoYacht.getColor());
         System.out.println("Fandango Yacht price: " + fandangoYacht.getPrice());
         System.out.println("Main sail color: " + fandangoYacht.getMainSailColor());
